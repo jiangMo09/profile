7cdfcc9755511a9137395dac86326afd8d0c0e8f
jiangMo09 <mmiao0641@gmail.com>
2024-02-26 03:35:41 +0800

    [FIX]next.config fix basePath

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
-      <img src="/images/frost-2.jpg" />
+      <img src="/profile/images/frost-2.jpg" />
     </div>
   );
 };
